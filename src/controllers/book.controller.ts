5dd850de42904bda9212a847d6ab8d24920a48a6
blissexodia <binnoley@gmail.com>
2025-11-20 08:37:10 +0545

    +add

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,38 +1,45 @@
-import { Request, Response } from "express";
+import { Request,Response } from "express";
+import{z} from 'zod';
 
-export type Book = {
-    id: string;
-    title: string;
-    date?: string; // optional
-}
+export  const BookSchema = z.object({
+    id: z.string().min(1,{message: "Book ID is required"}),
+    title: z.string().min(1,{message: "Book Title is required"}),
+    date: z.string().optional()
+});
+export type Book = z.infer<typeof BookSchema>; // TypeScript type from Zod schema
 
-export const books: Book[] = [
-    { id: 'B-1', title: '1984' },
-    { id: 'B-2', title: 'To kill a Mockingbird', date: '2015-12-10' }
-];
+// export type Book = {
+//     id: string;
+//     title: string;
+//     date?:string;
+// }
+export const book: Book[] = [
+        {id: 'B-1', title: '1984'},
+        {id: 'B-2', title: 'To kill a Mockingbird', date: '2015-12-10'}
+    ];
 
 export class BookController {
     createBook = (req: Request, res: Response) => {
-        const { id, title } = req.body; // destructuring
-        // const id = req.body.id;
-        if (id) {
-            return res.status(400).json({ message: "Book ID is required" });
+        const {id,title} = req.body; // destructing
+        // const id: string = req.body.id;
+        if(!id){
+            return res.status(400).json({message: "Book ID is required"});
         }
-        if (title) {
-            return res.status(400).json({ message: "Book title is required" });
+        if(!title){
+            return res.status(400).json({message: "Book Title is required"});
         }
-        const checkBook = books.find(book => book.id === id);
-        if (checkBook) {
-            return res.status(409).json({ message: "Book with this ID already exists" });
+        const checkBook = book.find(elem => elem.id === id);
+        if(checkBook){
+            return res.status(409).json({message: "Book ID already exists"});
         }
-        const newBook: Book = { id, title };
-        // same as { id: id, title: title }, if key and variable names are same
-        books.push(newBook);
+        const newBook : Book = {id, title};
+        //same as {id:id, title:title}, if key and variable name are same
+        book.push(newBook);
         return res.status(201).json(newBook);
-
     }
-    getBooks = (req: Request, res: Response) => {
-        const return_book: Book[] = books;
-        res.status(200).json(return_book);
+
+    getBooks = (req: Request, res: Response) =>{
+    const return_book: Book[] = book;
+    res.status(200).json(return_book);
     }
 }
