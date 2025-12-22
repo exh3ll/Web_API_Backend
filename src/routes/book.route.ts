42f1ca1c65d90ee004751aa78feaf2c36935682a
blissexodia <binnoley@gmail.com>
2025-12-22 09:54:46 +0545

    routes

--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -1,14 +1,19 @@
 import { Router, Request, Response } from "express";
 import { BookController } from "../controllers/book.controller";
 
-const router: Router = Router();
 const bookController = new BookController();
 
-router.get('/', bookController.getBooks);
+const router: Router = Router();
+
+router.get('/',bookController.getBooks);
+
+// make a router that handles GET request that takes bookid
+// /:bookid and calls bookController.getBookById
+// router.get('/:bookid', bookCOntroller.getBookById)
+
+router.get('/:bookid', bookController.getBookById);
+
+
+router.post('/',bookController.createBook);
 
 export default router;
-
-//make a router that handles GET request that takes bookid
-// Lbookid and calls bookController,getBookbyID
-// router.get('/:bookid", bookController.getBookbyID);
-router.get('/:bookid', bookController.getBookbyID);
